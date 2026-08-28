8ebc580e31f1ef57579aabe17761ea43d36c99e6
Prateek <hridayved07@gmail.com>
2026-08-28 17:21:52 +0000

    Add program to find distance between two user given coordinates.

---
+++ Semester_1/C/01_Basics/distance_formula.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    float x1,x2,y1,y2,dist;
+    printf("Enter the coordinates x1 and y1 :");
+    scanf ("%f %f",&x1,&y1);
+    printf("Enter the coordinates x2 and y2 :");
+    scanf ("%f %f",&x2,&y2);
+    dist = sqrt(pow(x1-x2,2)+pow(y1-y2,2));
+    printf("The distance between the coordinates is : %.2f \n",dist);
+    return 0;
+}
